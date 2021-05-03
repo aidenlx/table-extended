0f6253bc8d834160b4ff07faaa174af734d73a29
AidenLx <aiden.lx@outlook.com>
2021-05-03 09:49:34 +0800

    build: add node polyfill and set isDesktopOnly to true

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from '@rollup/plugin-typescript';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 const isProd = (process.env.BUILD === 'production');
 
@@ -15,18 +16,19 @@ if you want to view the source visit the plugins github repository
 export default {
   input: "src/main.ts",
   output: {
-    dir: '.',
-    sourcemap: 'inline',
+    dir: ".",
+    sourcemap: "inline",
     sourcemapExcludeSources: isProd,
-    format: 'cjs',
-    exports: 'default',
+    format: "cjs",
+    exports: "default",
     banner,
   },
-  external: ['obsidian'],
+  external: ["obsidian"],
   plugins: [
     typescript(),
-    nodeResolve({browser: true}),
+    nodeResolve({ browser: true }),
+    nodePolyfills({ include: ["punycode"] }),
     commonjs(),
-    json()
-  ]
+    json(),
+  ],
 };
